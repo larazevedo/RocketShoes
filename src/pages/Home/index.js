60f41227f519e5dfdf9ab4ec681fc0278afd2d4e
Lara <lara.azevedo@outlook.com>
2019-09-02 21:39:47 -0300

    Puxando dados da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,94 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://images.lojanike.com.br/landingpage/ni/184/10/assets/img/air-jordan-xxxiii-azul.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal!</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images.lojanike.com.br/landingpage/ni/184/10/assets/img/jordan-proto-react.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal!</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images.lojanike.com.br/320x320/produto/99904_783137_20190704203531.png"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal!</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images.lojanike.com.br/320x320/produto/96941_1559534_20190730081031.png"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal!</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images.lojanike.com.br/320x320/produto/89565_1224668_20190723100842.png"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal!</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images.lojanike.com.br/320x320/produto/4030_367610_20190607094009.png"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal!</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    // Formatando o preço uma única vez no início.
+    // Pra não colocar funções desnecessárias no render
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
