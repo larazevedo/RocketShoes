edeb8add9eafbff19666b087ea2182b8633844d9
Lara <lara.azevedo@outlook.com>
2019-09-03 17:08:46 -0300

    Adicionando ao carrinho - Action Redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -22,6 +23,17 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    // Dispatch: dispara uma action ao redux.
+    const { dispatch } = this.props;
+
+    // Quando dispara o dispatch, todos os reducers são ativados!
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -32,7 +44,10 @@ export default class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#" /> 3
               </div>
@@ -44,3 +59,20 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
+
+/*
+    Componente da Home: a partir do momento que conecta com o redux (usando o connect)
+ele tem acesso a uma propriedade chamada DISPATCH.
+    Esse dispatch é responsável por disparar as actions, que dizem pro redux que a gente quer
+fazer alguma alteração no nosso estado.
+    Na ação, obrigatoriamente, se passa um type.
+    Todos os reducers ouvem todas as actions, por isso a necessidade de usar um switch.
+    Nesse exemplo:
+  O carrinho está vazio, seta state = [], depois de ouvir a action ideal para adicionar um
+item no carrinho, retorna [ ...state], ou seja, já pega tudo o que tem no state(carrinho)
+e acrescenta o action.product.
+  Assim que foram feitas essas atualizações, o redux avisa todos os componentes que possuem o connect
+e que estão necessitando da informação daquele reducer e executa novamente com as informações novas.
+*/
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,11 @@
-export default function cart() {
-  return [];
+// state: estado anterior à alteração.
+export default function cart(state = [], action) {
+  // Switch garante que o reducer ouve somente a ação ADD TO CART
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    // Se não ouvir, deixa o estado do jeito q estava
+    default:
+      return state;
+  }
 }
